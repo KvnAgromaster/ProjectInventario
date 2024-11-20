ee5e657e3b44dc9c7a6cfdd9afd0b45e943e850b
KvnAgromaster <dev1@agromaster.com.mx>
2024-11-20 15:25:25 -0700

    Filtros tabla movimiento
    - Ahora se pueden buscar por filtros (almacen, producto, tipo movimiento)

--- vistas/modulos/inventario.php
+++ vistas/modulos/inventario.php
@@ -1,86 +1,166 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Inventario
-        <small>Panel de Control</small>
-      </h1>
-    </section>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Inventario
+      <small>Panel de Control</small>
+    </h1>
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
+  <!-- Main content -->
+  <section class="content">
 
+    <div class="actionContainer">
       <div class="box-header with-border">
-        <div>
-          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarMovimiento">
-            <i class="fa fa-plus"></i>
-             Nuevo Movimiento
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarMovimiento">
+          <i class="fa fa-plus"></i>
+          Nuevo Movimiento
+        </button>
+      </div>
+
+      <div class = "filter-container">
+
+        <div class="col-xs-6">
+          <select class="form-control" id="filtroAlmacen" name="filtroAlmacen" required>
+            <option value="">Todos Almacenes</option>
+
+            <?php
+
+              $item = null;
+              $valor = null;
+
+              $json = array("tabla" => "almacenes",
+                                "item" => $item,
+                                "valor" => $valor);
+
+              $datos = json_encode($json);
+
+              $almacenes = ModeloAlmacenes::mdlMostrarAlmacenes($datos);
+
+              foreach ($almacenes as $key => $value) {
+                
+                echo '<option value="'.$value["almacen"].'">'.$value["almacen"].'</option>';
+              }
+
+            ?>
+
+          </select>
+
+        </div>
+
+        <div class="col-xs-6">
+          <select class="form-control" id="filtroProducto" name="filtroProducto" required>
+            <option value="">Todos Productos</option>
+
+            <?php
+
+              $item = null;
+              $valor = null;
+
+              $json = array("tabla" => "productos",
+                                "item" => $item,
+                                "valor" => $valor);
+
+              $datos = json_encode($json);
+
+              $productos = ModeloProductos::mdlMostrarProductos($datos);
+
+              foreach ($productos as $key => $value) {
+                
+                echo '<option value="'.$value["producto"].'">'.$value["producto"].'</option>';
+              }
+
+            ?>
+
+          </select>
+
+        </div>
+
+        <div class="col-xs-6">
+          <select class="form-control" id="filtroMov" name="filtroMov" required>
+            <option value="">Todos Movimientos</option>
+            <option value="1">Entrada</option>
+            <option value="0">Salida</option>
+
+          </select>
+
+        </div>
+
+        <div class="box-header with-border">
+
+          <button class="btn btn-primary" id="btnFiltro">
+            <i class="fa fa-filter"></i>
+            FILTRAR
           </button>
         </div>
+
       </div>
+    </div>
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-body">
-          <div class="nav-tabs-custom">
-            <ul class="nav nav-tabs">
-              <li>
-                <a href="#tab_1" data-toggle="tab"><i class="fa fa-th-list"></i><strong> INVENTARIOS</strong></a>
-              </li>
+   
 
-              <li class="active">
-                <a href="#tab_2" data-toggle="tab"><i class="fa fa-exchange"></i><strong> MOVIMIENTOS</strong></a>
-              </li>
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-body">
+        <div class="nav-tabs-custom">
+          <ul class="nav nav-tabs">
+            <li>
+              <a href="#tab_1" data-toggle="tab"><i class="fa fa-th-list"></i><strong> INVENTARIOS</strong></a>
+            </li>
 
-            </ul>
-            <div class="tab-content">
+            <li class="active">
+              <a href="#tab_2" data-toggle="tab"><i class="fa fa-exchange"></i><strong> MOVIMIENTOS</strong></a>
+            </li>
 
-              <div class="tab-pane" id="tab_1">
-                <table class="table table-striped table-condensed" id="datatable_inventario">
-                  <thead>
-                    <th>#</th>
-                    <th>Almacen</th>
-                    <th>Producto</th>
-                    <th>Total Entradas</th>
-                    <th>Total Salidas</th>
-                    <th>Diferencia</th>
-                    <th>Acciones</th>
-                  </thead>
-                  <tbody>
-                    
-                  </tbody> 
-                </table>
-              </div>
+          </ul>
+          <div class="tab-content">
 
-              <div class="tab-pane active" id="tab_2">
-                <table class="table table-condensed" id="datatable_movimientos">
-                  <thead>
-                    <th>#</th>
-                    <th>Almacen</th>
-                    <th>Producto</th>
-                    <th>Cantidad</th>
-                    <th>Fecha</th>
-                    <th>Ultima Modificacion</th>
-                    <th>Acciones</th>
-                  </thead>
-                  <tbody>
-                    
-                  </tbody> 
-                </table>
-              </div>
-            </div>     
-          </div>
-      
+            <div class="tab-pane" id="tab_1">
+              <table class="table table-striped table-condensed" id="datatable_inventario">
+                <thead>
+                  <th>#</th>
+                  <th>Almacen</th>
+                  <th>Producto</th>
+                  <th>Total Entradas</th>
+                  <th>Total Salidas</th>
+                  <th>Diferencia</th>
+                  <th>Acciones</th>
+                </thead>
+                <tbody>
+                  
+                </tbody> 
+              </table>
+            </div>
+
+            <div class="tab-pane active" id="tab_2">
+              <table class="table table-condensed" id="datatable_movimientos">
+                <thead>
+                  <th>#</th>
+                  <th>Almacen</th>
+                  <th>Producto</th>
+                  <th>Cantidad</th>
+                  <th>Fecha</th>
+                  <th>Ultima Modificacion</th>
+                  <th>Acciones</th>
+                </thead>
+                <tbody>
+                  
+                </tbody> 
+              </table>
+            </div>
+          </div>     
         </div>
-
+    
       </div>
-      <!-- /.box -->
 
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    </div>
+    <!-- /.box -->
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 
 <div id="modalAgregarMovimiento" class="modal fade" role="dialog">
   <div class="modal-dialog">
